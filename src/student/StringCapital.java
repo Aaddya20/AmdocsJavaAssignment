38471708db66638d62542b844836dae25cc4a476
amdjfsim11 <amdjfsim11@AmdJava22.iiht.tech>
2022-09-16 12:29:04 +0530

    initial commit

---
+++ src/student/StringCapital.java
@@ -0,0 +1,29 @@
+package student;
+
+public class StringCapital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "i love java";
+		
+		char f = str.charAt(0);
+		String newStr = String.valueOf(Character.toUpperCase(f));
+		String result = newStr;
+		
+		for(int i=1; i<str.length(); i++)
+		{
+			if(str.charAt(i)==32)
+			{
+				f = str.charAt(i+1);
+				newStr = String.valueOf(Character.toUpperCase(f));
+				result += " "+newStr;
+				i++;
+			}
+			else
+				result += str.charAt(i);
+		}
+		System.out.println(result);
+	}
+
+}
